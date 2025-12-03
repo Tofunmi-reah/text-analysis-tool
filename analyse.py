d31e2968f4a4e6ddce8b40c682f9d75e8f54f63c
Tofunmi reah <oluwasolapeilesanmi@gmail.com>
2025-12-03 22:51:14 +0100

    obtained the username

---
+++ analyse.py
@@ -0,0 +1,10 @@
+print("welcome to the text analyse tool, I will mine and analyse a body of text from a file you give me!")
+
+# Get Username
+
+# Print message prompting the user to input their name
+# Get input from user to the terminal
+
+username = input("\nTo begin, Please Enter Your Username:\n")
+
+print('Hello, ' + username)
